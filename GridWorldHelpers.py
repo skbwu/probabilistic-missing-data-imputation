0479a04caaf24857e1bbd3ee9f35fc1f0f37d90f
KChaz <kec89@cornell.edu>
2024-04-08 12:33:03 -0400

    Move missing data mechanisms out of notebook and into more permanent helper script

--- GridWorldHelpers.py
+++ GridWorldHelpers.py
@@ -262,4 +262,133 @@ def sync_color(state, gw_colors):
     
     
     
+######################################
+# Missing Data Mechanism
+######################################  
+
+def MCAR(state, thetas):
+    """
+    This is the building block missing data function which, 
+    given the current state and a vector of thetas of the same length,
+    independently and randomly sets each element i of the vector to missing
+    with Bernoulli(theta_i). That is, thetas represent the probability
+    of being missing so 
     
+        * \theta_i = 0   for always observed
+        * \theta_i = 1   for never observed
+
+    Parameters
+    ----------
+    state : np array
+    thetas : np array of same length as state with elements in [0,1]
+
+    Returns
+    -------
+    po_state : a copy of state, possibly with some elements set to np.nan
+    """
+    assert len(state) == len(thetas), "theta-state length mismatch"
+    
+    # make a copy to stay safe: "partially-observed state"
+    po_state = state.copy().astype(float)
+    
+    # generate bernoullis and mask relevant elements
+    mask = np.random.binomial(1, thetas).astype(bool)
+    po_state[mask] = np.nan
+    
+    # return our missing-operated state
+    return po_state
+
+
+def Mcolor(state, theta_dict): 
+    """
+    Applied MCAR function  (see above) with a different theta
+    for each color
+    
+    If the third element of each theta entry is 0 so that
+    color can never be missing, then this is MAR. Else, this is NMAR
+    
+    Examplantion: x,y|c are missing at random indepenent of the true
+    value of (x,y) but c|x,y will depend on the missing value of c
+    if color is missing. E.g., if red is has high missing rates
+    and green has low missing rates, then we may assess the probability
+    of red|x,y to be lower than it actually is just because of higher missing
+    rate.
+    
+    Note: this becomes especially relevant when we have stochastic
+    water so that color is random...some areas of our grid example have
+    constant color, which shouldn't be hard to learn ( maybe depending
+    a bit on initialization )
+    
+    
+    Parameters
+    ----------
+    state : np.array
+    
+    theta_dict : dictionary with keys "0","1","2" and values
+        each an np.array of same length as state containing
+        elements in [0,1]
+
+    Returns
+    -------
+    po_state : a copy of state, possibly with some elements set to np.nan
+  
+    """
+    # query what our true color is + get the corresponding thetas_c vector
+    c = int(state[2]); thetas_c = theta_dict[c]
+    
+    # use MCAR
+    return MCAR(state, thetas_c)
+
+
+
+
+# If you are in that region, 
+# Mfog is MAR if theta_{1j} = theta_{2j} = 0 (i.e, x,y are observed for all timesteps), else NMAR
+def Mfog(state, i_range, j_range, thetas_in, thetas_out):
+    """
+    This missing data mechanism 'casts a fog' over some
+    rectangular region of the grid so that within this region, 
+    missingness has one rate and outside this region, another.
+    
+    This is MCAR if (x,y) are always observed (first two elements of
+         each thet aare 0) and only color is missing
+    Else it is NMAR
+    
+    
+    Parameters
+    ----------
+    state : np.array
+
+    i_range : a tuple (a,b) for lower and upper bounds of fog in y direction
+   
+    j_range : a tuple (c,d) for lower and upper bounds of fog in x direction
+        DESCRIPTION.
+     
+    thetas_in : np array of same length as state with elements in [0,1]
+  
+    thetas_out : np array of same length as state with elements in [0,1]
+
+    Returns
+    -------
+    po_state : a copy of state, possibly with some elements set to np.nan
+ 
+    """
+    assert len(state) == len(thetas_in), "thetas_in length mismatch"
+    assert len(state) == len(thetas_out), "thetas_out length mismatch"
+    
+    # default to not being in the region
+    inregion = False
+    
+    # check if we're in the fog region or not
+    if np.clip(a=state[0], a_min=i_range[0], a_max=i_range[1]) == state[0]:
+        if np.clip(a=state[1], a_min=j_range[0], a_max=j_range[1]) == state[1]:
+            inregion = True
+            
+    # figure out what thetas to use + apply the MCAR
+    thetas = thetas_in if inregion else thetas_out
+    return MCAR(state, thetas)
+    
+
+
+
+
--- GridWorldTests.py
+++ GridWorldTests.py
@@ -2,3 +2,47 @@
 """
 Script for tests of our functions
 """
+import numpy as np
+
+import GridWorldHelpers as gwh
+
+
+def test_miss_mech():
+    """
+    Some very basic sanity check tests. Could do more to make sure
+    actually doing things at right rate but have in any case done some
+    repeated draws to make sure things looked right
+    """
+
+    # set-up
+    theta_dict = {0: np.array([1,1,1]),
+                       1: np.array([0.0,0.0,0.0]),
+                       2: np.array([.4,.4,.4])}
+    
+    
+    i_range = (3,5); j_range = (3,5)
+    thetas_in = np.array([1,1,1])
+    thetas_out = np.array([0.0,0.0,0.0]) 
+    
+    
+    # test of basic MCAR
+    state = np.array([1,1,1])
+    assert (gwh.MCAR(state,np.array([0,0,0])) == state).all()
+    assert np.isnan(gwh.MCAR(state,np.array([1,1,1]))).all()
+
+    
+    # test of color
+    assert (gwh.Mcolor(state,theta_dict) == state).all()
+    state[2] = 0
+    assert np.isnan(gwh.Mcolor(state,theta_dict)).all()
+    
+    # test of fog - out region  
+    out = gwh.Mfog(state, i_range = i_range, j_range = j_range, thetas_in = thetas_in, thetas_out = thetas_out)
+    assert (out == state).all()
+    
+    # test of fog - in region
+    out = gwh.Mfog(np.array([4,4,1]), i_range = i_range, j_range = j_range, thetas_in = thetas_in, thetas_out = thetas_out)
+    assert np.isnan(out).all()
+   
+    
+    print("All tests passed")
